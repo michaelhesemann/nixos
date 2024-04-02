15b55cd69301cf87167fad2b71d310c0e4168b8e
Michael Hesemann <michael@mhesemann.de>
2024-04-02 23:15:50 +0200

    a

--- modules/user-server.nix
+++ modules/user-server.nix
@@ -9,4 +9,8 @@
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4OLzp+BiTyzXu0lP/6PIJjds3kZxqxp0U7j1AKFuwq" ];
   };
+
+  users.users.root = {
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4OLzp+BiTyzXu0lP/6PIJjds3kZxqxp0U7j1AKFuwq" ];
+  };
 }
