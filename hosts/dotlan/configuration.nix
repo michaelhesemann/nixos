e18e11a8cf1e384a12d8f92331006affd738096a
Michael Hesemann <michael@mhesemann.de>
2024-04-02 22:39:27 +0200

    a

--- hosts/dotlan/configuration.nix
+++ hosts/dotlan/configuration.nix
@@ -7,7 +7,7 @@ let
 
   mysqlPkgs = import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
-      sha256 = "aaaa";
+      sha256 = "";
   }) {};
 in
 {
