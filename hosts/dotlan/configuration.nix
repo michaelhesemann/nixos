d5c494d79792089f63a16a4854f7862fd75d906f
Michael Hesemann <michael@mhesemann.de>
2024-04-02 23:00:31 +0200

    a

--- hosts/dotlan/configuration.nix
+++ hosts/dotlan/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
   phpPkgs = import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
