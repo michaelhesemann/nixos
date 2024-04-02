3ad0fa08f9f8e82d031d603093efeecc3dd250b4
Michael Hesemann <michael@mhesemann.de>
2024-04-02 22:08:37 +0200

    a

--- hosts/dotlan/configuration.nix
+++ hosts/dotlan/configuration.nix
@@ -2,10 +2,12 @@
 let
   phpPkgs = import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
+      sha256 = "aec0b610b9f6026c873f778a2e9c48ecfe84b7e1cba57015975176bfa4984379";
   }) {};
 
   mysqlPkgs = import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
+      sha256 = "92639d1247beb37c6e3cbcf835fa69160fa72ed53ffce69c70c40b1b608c9ef3";
   }) {};
 in
 {
