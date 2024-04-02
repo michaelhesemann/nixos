5cd234293bfd353a295eb536225b5239f65d56a1
Michael Hesemann <michael@mhesemann.de>
2024-04-02 23:19:57 +0200

    a

--- hosts/dotlan/configuration.nix
+++ hosts/dotlan/configuration.nix
@@ -1,15 +1,15 @@
 { config, pkgs, ... }:
-# let
-#   phpPkgs = import (builtins.fetchTarball {
-#       url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
-#       sha256 = "0wpl8rhkfd7s4nm32vfn6fha0g4a1p5214117vvwsl4y3l1gm68a";
-#   }) {};
+let
+  phpPkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
+      sha256 = "0wpl8rhkfd7s4nm32vfn6fha0g4a1p5214117vvwsl4y3l1gm68a";
+  }) {};
 
-#   mysqlPkgs = import (builtins.fetchTarball {
-#       url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
-#       sha256 = "";
-#   }) {};
-# in
+  mysqlPkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
+      sha256 = "";
+  }) {};
+in
 {
   imports = [
     ./hardware-configuration.nix
@@ -27,14 +27,13 @@
 
   # sshd
   services.sshd.enable = true;
-  # services.openssh.settings.permitRootLogin = "yes";
 
   # system-wide packages
   environment.systemPackages = with pkgs; [
     neovim
     git
-    # phpPkgs.php56
-    # mysqlPkgs.mysql80
+    phpPkgs.php56
+    mysqlPkgs.mysql80
   ];
 
   # networking
