9c969c560e5a699c6da9d79a7e60824d7a71c8cd
Michael Hesemann <michael@mhesemann.de>
2024-04-04 01:36:54 +0200

    a

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, system, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
