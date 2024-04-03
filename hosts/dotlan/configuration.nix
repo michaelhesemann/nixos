cf7fcd320325e8da4ca2f378ec21ff1ebbed4366
Michael Hesemann <michael@mhesemann.de>
2024-04-04 01:42:20 +0200

    a

--- hosts/dotlan/configuration.nix
+++ hosts/dotlan/configuration.nix
@@ -1,16 +1,19 @@
 { config, pkgs, inputs, ... }:
-# let
-#   phpPkgs = import (builtins.fetchTarball {
-#       url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
-#       sha256 = "0wpl8rhkfd7s4nm32vfn6fha0g4a1p5214117vvwsl4y3l1gm68a";
-#   }) {};
+let
+  phpPkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
+      sha256 = "0wpl8rhkfd7s4nm32vfn6fha0g4a1p5214117vvwsl4y3l1gm68a";
+  }) {};
 
-#   mysqlPkgs = import (builtins.fetchTarball {
-#       url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
-#       sha256 = "";
-#   }) {};
+  mysqlPkgs = import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
+      sha256 = "0qij2z6fxlmy4y0zaa3hbza1r2pnyp48pwvfvba614mb8x233ywq";
+  }) {};
 
-# in
+  phpPkgs = phpPkgs.php56;
+  mysqlPkgs = mysqlPkgs.mysql80;
+
+in
 {
   imports = [
     ./hardware-configuration.nix
