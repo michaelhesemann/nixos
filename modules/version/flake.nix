854b0891c2efbfb6951e68a91da38aa85a496335
Michael Hesemann <michael@mhesemann.de>
2024-04-04 02:00:30 +0200

    a

--- modules/version/flake.nix
+++ modules/version/flake.nix
@@ -14,14 +14,6 @@ in
           sha256 = "0wpl8rhkfd7s4nm32vfn6fha0g4a1p5214117vvwsl4y3l1gm68a";
         };
       });
-
-      mysql80 = super.mysql80.overrideAttrs (oldAttrs: {
-        version = "8.0.34";
-        src = fetchurl {
-          url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
-          sha256 = "0qij2z6fxlmy4y0zaa3hbza1r2pnyp48pwvfvba614mb8x233ywq";
-        };
-      });
     })
   ];
 }
