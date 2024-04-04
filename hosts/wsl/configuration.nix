0dc2821560c0f1923ebdffd9518b69e932758306
Michael Hesemann <michael.hesemann@sva.de>
2024-04-04 15:04:19 +0200

    a

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -1,14 +1,15 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports = [
+    # include NixOS-WSL modules
     <nixos-wsl/modules>
   ];
 
   wsl.enable = true;
   wsl.defaultUser = "nixos";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system = "x86_64-linux";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [
     git
@@ -16,5 +17,11 @@
     php
   ];
 
-  system.stateVersion = "23.11";
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
 }
