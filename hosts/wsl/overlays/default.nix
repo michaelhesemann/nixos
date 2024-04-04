4572dfacf47e34dde15855b75cc4b4b03130ade2
Michael Hesemann <michael.hesemann@sva.de>
2024-04-04 15:57:24 +0200

    a

--- hosts/wsl/overlays/default.nix
+++ hosts/wsl/overlays/default.nix
@@ -3,14 +3,14 @@
 
 {
   nixpkgs.overlays = [
-    (final: prev: {
-      php = prev.php.overrideAttrs (old: {
-        src = prev.fetchurl {
-          url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
-          sha256 = "rsC2ELn2AmyHP3eKLpxI7P6Et+HLpXAVl1F2v6SYQ3k=";
-        };
-      });
-    })
+    # (final: prev: {
+    #   php = prev.php.overrideAttrs (old: {
+    #     src = prev.fetchurl {
+    #       url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
+    #       sha256 = "rsC2ELn2AmyHP3eKLpxI7P6Et+HLpXAVl1F2v6SYQ3k=";
+    #     };
+    #   });
+    # })
 
     (final: prev: {
       mysql = prev.php.overrideAttrs (old: {
