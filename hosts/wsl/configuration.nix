356085b2659c6ef1c0ac134d491e736b200489b0
Michael Hesemann <michael.hesemann@sva.de>
2024-04-04 15:05:58 +0200

    a

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -8,6 +8,7 @@
 
   wsl.enable = true;
   wsl.defaultUser = "nixos";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [
