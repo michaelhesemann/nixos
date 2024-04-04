e205102808a3866ccfaaefce7a552e2d5ab4b0d3
Michael Hesemann <michael.hesemann@sva.de>
2024-04-04 15:29:33 +0200

    a

--- flake.nix
+++ flake.nix
@@ -3,14 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-php-5-6-36 = {
-      url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
-      sha256 = "0wpl8rhkfd7s4nm32vfn6fha0g4a1p5214117vvwsl4y3l1gm68a";
-    };
-    nixpkgs-mysql-8-0-43 = {
-      url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
-      sha256 = "0qij2z6fxlmy4y0zaa3hbza1r2pnyp48pwvfvba614mb8x233ywq";
-    };
+    nixpkgs-php-5-6-36.url = "https://github.com/NixOS/nixpkgs/archive/a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0.tar.gz";
+    nixpkgs-mysql-8-0-43.url = "https://github.com/NixOS/nixpkgs/archive/5a8650469a9f8a1958ff9373bd27fb8e54c4365d.tar.gz";
   };
 
   outputs = { self, nixpkgs, nixpkgs-php-5-6-36, nixpkgs-mysql-8-0-43, ... }@inputs:
