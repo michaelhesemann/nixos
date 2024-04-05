b535e2d26777a3aa57dd0cce96717da3cefb4905
Michael Hesemann <michael.hesemann@sva.de>
2024-04-05 12:50:53 +0200

    a

--- package-versions/mysql.nix
+++ package-versions/mysql.nix
@@ -1,12 +1,12 @@
 { config, pkgs, nixpkgs, ... }:
 
 let
-  pkgs = import <nixpkgs> { system = "x86_64-linux"; }; (builtins.fetchGit {
+  pkgs = import (builtins.fetchGit {
     name = "mysql-8-0-34";
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/heads/nixpkgs-unstable";
     rev = "9957cd48326fe8dbd52fdc50dd2502307f188b0d";
-  }) {};
+  }) { system = "x86_64-linux"; };
 
   mysql = pkgs.mysql80;
 in
--- package-versions/php.nix
+++ package-versions/php.nix
@@ -1,12 +1,12 @@
 { config, pkgs, nixpkgs, ... }:
 
 let
-  pkgs = import <nixpkgs> { system = "x86_64-linux"; }; (builtins.fetchGit {
+  pkgs = import (builtins.fetchGit {
     name = "php-5-6-36";
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/heads/nixpkgs-unstable";
     rev = "a5c9c6373aa35597cd5a17bc5c013ed0ca462cf0";
-  }) {};
+  }) { system = "x86_64-linux"; };
 
   php = pkgs.php56;
 in
